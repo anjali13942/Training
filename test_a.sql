9a8bea044e7421c03a79d4ac27cc81062b8ee431
Anjali Devi <devi.anjali96@gmail.com>
2019-09-27 17:07:04 +0530

    test a file change

--- test_a.sql
+++ test_a.sql
@@ -7,6 +7,8 @@ id2 integer NOT NULL
 );
 CREATE SEQUENCE IF NOT EXISTS test.a_id_seq
  AS integer;
+ CREATE SEQUENCE IF NOT EXISTS test.a_id_seq
+ AS integer;
 ALTER TABLE test.a_id_seq OWNER TO postgres;
 ALTER SEQUENCE test.a_id_seq OWNED BY test.a.id;
 ALTER TABLE test.a ALTER COLUMN id SET DEFAULT nextval('test.a_id_seq1'::regclass);
